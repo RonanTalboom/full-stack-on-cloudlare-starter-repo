5072f001c45c00d9c8905f136453fb9d45bcd718
Ronan <ronan@Mac-mini-de-Ronan.local>
2025-08-24 22:04:37 +0200

    Implement routing and caching for link information with KV storage integration

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -8,11 +8,10 @@ import {
 import { TRPCError } from "@trpc/server";
 import {
   ACTIVE_LINKS_LAST_HOUR,
-  LAST_30_DAYS_BY_COUNTRY,
-  LINK_LIST,
+  LAST_30_DAYS_BY_COUNTRY
 } from "./dummy-data";
 
-import { createLink } from "@repo/data-ops/queries/links";
+import { createLink, getLink, getLinks, updateLinkDestinations, updateLinkName } from "@repo/data-ops/queries/links";
 
 export const linksTrpcRoutes = t.router({
   linkList: t.procedure
@@ -21,8 +20,8 @@ export const linksTrpcRoutes = t.router({
         offset: z.number().optional(),
       }),
     )
-    .query(async ({}) => {
-      return LINK_LIST;
+    .query(async ({ctx, input}) => {
+      return await getLinks(ctx.userInfo.userId, input.offset?.toString());
     }),
   createLink: t.procedure.input(createLinkSchema).mutation(async ({ctx, input}) => {
     const linkId = await createLink({
@@ -40,6 +39,7 @@ export const linksTrpcRoutes = t.router({
     )
     .mutation(async ({ input }) => {
       console.log(input.linkId, input.name);
+      await updateLinkName(input.linkId, input.name)
     }),
   
   getLink: t.procedure
@@ -48,19 +48,8 @@ export const linksTrpcRoutes = t.router({
         linkId: z.string(),
       }),
     )
-    .query(async ({}) => {
-      const data = {
-        name: "My Sample Link",
-        linkId: "link_123456789",
-        accountId: "user_987654321",
-        destinations: {
-          default: "https://example.com",
-          mobile: "https://mobile.example.com",
-          desktop: "https://desktop.example.com",
-        },
-        created: "2024-01-15T10:30:00Z",
-        updated: "2024-01-20T14:45:00Z",
-      };
+    .query(async ({input}) => {
+      const data = await getLink(input.linkId) 
       if (!data) throw new TRPCError({ code: "NOT_FOUND" });
       return data;
     }),
@@ -72,7 +61,7 @@ export const linksTrpcRoutes = t.router({
       }),
     )
     .mutation(async ({ input }) => {
-      console.log(input.linkId, input.destinations);
+      await updateLinkDestinations(input.linkId, input.destinations)
     }),
   activeLinks: t.procedure.query(async () => {
     return ACTIVE_LINKS_LAST_HOUR;
